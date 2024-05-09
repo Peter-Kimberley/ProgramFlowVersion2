e367a84c0f2f6be8e2bfe5133014640fe762043e
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-09 08:54:53 +0100

    Have added extra conditions to the code

--- Blocks.py
+++ Blocks.py
@@ -1,5 +1,16 @@
 name = input("Please enter your name: ")
-age = int("How old are yoy, {0}? ". format (name))
+age = int(input("How old are yoy, {0}? ". format (name)))
 print(age)
 
-# using the int and the input function to getr answers from the user #
+if age >= 18:
+    print("You are old enough to vote")
+    print("Please put an X in the box")
+else:
+    print("Please come back in {0} years".format(18 - age)) 
+
+if age <= 18:
+    print("Please come back in {0} years".format(18 - age)) 
+else:
+    print("You are old enough to vote")
+    print("Please put an X in the box")
+# There are more conditions added to the code #     
