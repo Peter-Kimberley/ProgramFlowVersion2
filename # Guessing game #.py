ef9e1184872c151314aa01c6d507e00254141798
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-10 23:17:32 +0100

    Have now added a second guess option aswell as an additional message

--- # Guessing game #.py
+++ # Guessing game #.py
@@ -1,11 +1,25 @@
 # Guessing game #
 answer = 5
+
 print("Please guess a number betweem 1 and 10: ")
 guess = int(input())
+
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())
+    if guess  == answer:
+        print("well done you guessed it")
+    else:
+        print("Sorry you have not guessed correctly")
+
 elif guess > answer:
     print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("well done you guessed it")
+    else:
+        print("Sorry you have not guessed correctly")
+
 else:
     print("You got it first time")
 
