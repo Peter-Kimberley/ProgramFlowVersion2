7cf48da7daad2f392abd9e2452dddd7c223d5bd5
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-10 23:01:17 +0100

    This is a file to compare x to y

---
+++ # x compared t y.py
@@ -0,0 +1,13 @@
+# x compared t y
+# This piece of code makes a comparison between the value of x and y and then prints statements accordingly
+
+# Changing the variable amounts will output the according response.
+x = 7
+y = 7
+
+if x > y:
+    print("x is greater than y ")
+elif x < y:
+    print("x is smaller than y ")
+else:
+    print("x equals y ")
