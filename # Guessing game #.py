c802fe7e7e6d132a6dd9e5fac6acd132ca757542
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-12 08:50:21 +0100

    # Guessing game is now simplified to allow to check if answer is correct first time.

--- # Guessing game #.py
+++ # Guessing game #.py
@@ -1,26 +1,40 @@
-# Guessing game #
 answer = 5
 
-print("Please guess a number betweem 1 and 10: ")
+print("Please guess number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher")
-    guess = int(input())
-    if guess  == answer:
-        print("well done you guessed it")
-    else:
-        print("Sorry you have not guessed correctly")
-
-elif guess > answer:
-    print("Please guess lower")
-    guess = int(input())
-    if guess == answer:
-        print("well done you guessed it")
-    else:
-        print("Sorry you have not guessed correctly")
-
-else:
+if guess == answer:
     print("You got it first time")
-
+else:
+    if guess < answer:
+     print("Please guess higher")
+    else:
+       print("Please guess lower")
+    guess = int((input))
+    if guess == answer:
+       print("Well done you guessed it")
+    else:
+       print("Sorry you didnt guees correctly")
+       
     
+# Guessing game is now simplified to allow to check if answer is correct first time.
+
+
+
+# if guess < answer:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("You got it first time")
+
