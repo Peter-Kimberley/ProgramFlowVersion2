65704c69e2e07537a4f62e329cf46899da911b29
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-12 09:15:58 +0100

    Using conditions to check age

--- # Guessing game #.py
+++ # Guessing game #.py
@@ -38,3 +38,4 @@ else:
 # else:
 #     print("You got it first time")
 
+
---
+++ conditions.py
@@ -0,0 +1,15 @@
+# Using conditions to check age 
+
+age = int(input("How old are you? "))
+
+if age >= 16 and age <= 65:
+   if age >= 16 and age <= 65:
+      print("Have a good day at work")
+else:
+     print("Ejoy your free time")
+
+print("***" * 80)
+if age < 16 or age > 65:
+   print("Enjoy your free time")
+else:
+    print("Have a good da at work")
