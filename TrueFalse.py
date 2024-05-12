75453c35d0d0d613e285a684748f19943117b778
Peter Kimberley <p3t3rk1m0@gmail.com>
2024-05-12 11:46:50 +0100

    True and False conditions

---
+++ TrueFalse.py
@@ -0,0 +1,12 @@
+day = "Saturday"
+temperature = 30
+raining = True
+
+if day == ("Saturday" and temperature > 27) and not raining:
+    print("Go swimming")
+else:
+    print("Stay in and learn Python")
+
+# True and False conditions determine 
+# the relavent outputed text.
+# Also making use of the () symbols, as in the order of preference
