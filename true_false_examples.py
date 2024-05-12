52a49b1462a5a28d454b5f1e27f19f1981740dd6
Peter Kimberley <p3t3rk1m0@gmail.com>
2024-05-12 17:21:49 +0100

    An example of truth vaues within Python

---
+++ true_false_examples.py
@@ -0,0 +1,14 @@
+# This is anotehr example of true false values 'Tuthy values'
+if 0 :
+    print("True")
+else:
+    print("False")
+name = input("Please enter your name: ")
+# if name:This is a check for truth but is not relly easy to underatnd,
+#this can be re-written to make more sense as written below
+if name != "": 
+    print("Hello {} ".format(name))
+else:
+    print("Are you the man with no name?")
+
+
