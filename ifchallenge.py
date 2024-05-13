beb2be3e2333426027e78f63e7febb43f25a8f19
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-13 19:04:55 +0100

    This is a piece of code that tests the age of the user and checks there agae and then compares it to a 18 - 30 holiday

---
+++ ifchallenge.py
@@ -0,0 +1,6 @@
+age = int(input("How old are you? "))
+
+if age <= 30 and age >= 18:
+    print("Welcome to the holiday")
+else:
+    print("I am sorry but you do not meet the age restriction for this holiday" )
