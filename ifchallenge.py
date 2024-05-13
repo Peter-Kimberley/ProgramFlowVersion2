06fe32706038602d84f46d12311902d0e4dbfa0a
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-13 19:12:49 +0100

    I have added a line of code to aske for the name then added the .format function to strings that give the replies

--- ifchallenge.py
+++ ifchallenge.py
@@ -1,6 +1,10 @@
-age = int(input("How old are you? "))
+name = input("What is you name?: ")
+age = int(input("How old are you?: "))
 
 if age <= 30 and age >= 18:
-    print("Welcome to the holiday")
+    print("Welcome {} to the holiday".format(name))
 else:
-    print("I am sorry but you do not meet the age restriction for this holiday" )
+    print("I am sorry {} but you do not meet the age restriction for this holiday".format(name) )
+
+
+# Added a line of code asking for the name and added this to bot string outputs as a .fornmat for strings
