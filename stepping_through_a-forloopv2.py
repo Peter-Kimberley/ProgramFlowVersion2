4f47ea977a86294b857fc6fdc4e40a8c7e3e0aac
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-14 19:39:02 +0100

    this is steeping_through_a_forloop with a minor change, we have added the option to get user input for the nuymber string and we have also added the function sum() to add up all the numbers

---
+++ stepping_through_a-forloopv2.py
@@ -0,0 +1,11 @@
+number = input("Please enter a series of numbers, using any characters to seperator that is not a number: ")
+seperators = ""
+
+for char in number:
+    if not char.isnumeric():
+        seperators = seperators + char
+# This is a check to see if the value of "isnumeric is false, when this checks out as false, then the char is added to seperators"
+#print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print(sum([int(val) for val in values]))
