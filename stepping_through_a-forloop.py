030c1bc626af281ff228ff9caa8530368c6ccf20
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-14 19:17:49 +0100

    this checks for none numerical values in a for loop and then adds them to a variable seperators

---
+++ stepping_through_a-forloop.py
@@ -0,0 +1,11 @@
+number = "9,223;372:036 854,775;807"
+seperators = ""
+
+for char in number:
+    if not char.isnumeric():
+        seperators = seperators + char
+# This is a check to see if the value of "isnumeric is false, when this checks out as false, then the char is added to seperators"
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
