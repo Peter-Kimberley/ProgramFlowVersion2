7fa9e0e89ea8c0c05a0b7c06852d9ae9d80b9e33
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-16 08:33:48 +0100

    Shippoing list modified and added new code searching

--- shopping.py
+++ shopping.py
@@ -6,5 +6,11 @@ print("--------")
     
 for item in shopping_list:
     if item == "printer":
-        continue
+        continue # This will continue if the condition is returned true, and will not be added to the code
+    print("Buy " + item)
+print("--------")
+
+for item in shopping_list:
+    if item == "printer":
+        break # added a break point for the code, this is good if you want to search through a list of a 1000 items
     print("Buy " + item)
