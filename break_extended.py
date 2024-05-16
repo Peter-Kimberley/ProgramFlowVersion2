a13e8b9cd875fd1036781628e31eeced6c249d65
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-05-16 19:38:23 +0100

    This is a change to the break code and have added a not None function

---
+++ break_extended.py
@@ -0,0 +1,20 @@
+shopping_list = ["Keyboard", "mouse", "monitor", "Mouse matt", "printer", "Paper"]
+
+item_to_find = "spam"
+found_at = None
+
+# for index in range(len(shopping_list)): # The len allows us to see how many items are in the list
+#     if shopping_list[index]  == item_to_find:
+#         found_at = index
+#         break
+
+
+
+if item_to_find in shopping_list:
+    found_at = shopping_list.index(item_to_find)
+if found_at is not None:
+    print("Item found at position {}".format(item_to_find))
+if found_at is not None:
+    print("Item found at position {}".format(found_at))
+else:
+    print("{} not found". format(item_to_find))
